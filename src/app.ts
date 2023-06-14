d44af85c601de69a4a32efea6c12d925c8ae3907
liujuncai <1769031928@qq.com>
2023-06-14 15:21:37 +0800

    feat: test google auth

--- src/app.ts
+++ src/app.ts
@@ -4,35 +4,53 @@ import { config } from './config/mysql/index'
 import Redis from 'ioredis'
 import mysql from 'mysql'
 import { querry } from './config/mysql/index'
-
+import session from 'koa-session'
+import passport from 'koa-passport'
 const app = new Koa()
 
-const redis = new Redis({
-  port: 6379,
-  host: '127.0.0.1',
-})
+import './config/googleAuth2/index'
+// const redis = new Redis({
+//   port: 6379,
+//   host: '127.0.0.1',
+// })
 
-const pool = mysql.createPool({
-  host: config.database.HOST,
-  user: config.database.USERNAME,
-  password: config.database.PASSWORD,
-  database: config.database.DATABASE,
-})
+// const pool = mysql.createPool({
+//   host: config.database.HOST,
+//   user: config.database.USERNAME,
+//   password: config.database.PASSWORD,
+//   database: config.database.DATABASE,
+// })
 
 const router = new Router()
 
 router.get('/', async (ctx: { body: string }, next: any) => {
-  const redisData = await redis.get('myKey')
-  const data = await querry('select * from users')
-  if (redisData) {
-    const a = 123
-    console.log(redisData, 'redisDatad')
-  } else {
-    void redis.set('myKey', 222)
-  }
-  console.log(redisData, 'redisData', data)
+  // const redisData = await redis.get('myKey')
+  // const data = await querry('select * from users')
+  // if (redisData) {
+  //   const a = 123
+  //   console.log(redisData, 'redisDatad')
+  // } else {
+  //   void redis.set('myKey', 222)
+  // }
+  // console.log(redisData, 'redisData', data)
   ctx.body = 'hello KOAaa'
 })
+
+// ===>
+
+app.keys = ['your-session-secret']
+app.use(session(app))
+app.use(passport.initialize())
+app.use(passport.session())
+
+router.get('/auth/google', passport.authenticate('google', { scope: ['profile'] }))
+
+router.get('/auth/google/callback', passport.authenticate('google', { failureRedirect: '/login' }), function (ctx) {
+  // Successful authentication, redirect home.
+  ctx.redirect('/')
+})
+
+// ===>
 
 app.use(router.routes())
 app.listen(3000, () => {
---
+++ src/config/googleAuth2/index.ts
@@ -0,0 +1,20 @@
+import passport from 'koa-passport'
+import { Strategy as GoogleStrategy } from 'passport-google-oauth20'
+
+const GOOGLE_CLIENT_ID = '1095594983604-8noehb0pm6hhf97u7sgvg793noisjjup.apps.googleusercontent.com'
+const GOOGLE_CLIENT_SECRET = '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'
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: GOOGLE_CLIENT_ID,
+      clientSecret: GOOGLE_CLIENT_SECRET,
+      callbackURL: 'http://localhost:3000/auth/google/callback',
+    },
+    function (accessToken, refreshToken, profile, cb) {
+      // Callback function to handle user profile data
+      // and store it in the database or session
+      console.log(' Callback function to handle user profile data')
+      cb(null, profile)
+    }
+  )
+)
